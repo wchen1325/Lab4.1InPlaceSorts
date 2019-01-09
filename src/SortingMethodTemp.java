571657d4fd3e19f250bd58fd200ff5c136e97dd1
isoha2749 <isoha2749@bths.edu>
2019-01-09 14:38:48 -0500

    new java class

---
+++ src/SortingMethodTemp.java
@@ -0,0 +1,8 @@
+public class SortingMethodTemp {
+    public static void selectionSort(int[] string){
+        int minPos = 0;
+        int curPos = 0;
+        for (int curPos = 0; curPos < string.length-1; curPos++)
+    }
+
+}
